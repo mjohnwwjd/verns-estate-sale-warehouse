99d27708cfa3ad57a05558b1a87905e08c6dcdc7
Mike's Mac <mikesmac@Mikess-Mac-mini.local>
2026-06-02 15:41:39 -0400

    Revise hero actions and add Meet Vern

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "verns-estate-sale-warehouse-v20260602-estate-fb-map-polish";
+const CACHE_NAME = "verns-estate-sale-warehouse-v20260602-meet-vern-hero";
 const APP_SHELL = [
   "./",
   "./index.html",
