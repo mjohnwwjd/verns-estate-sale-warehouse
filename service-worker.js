f663a10301447cfa0a665a15e9d7256377c5f53f
Mike's Mac <mikesmac@Mikess-Mac-mini.local>
2026-08-03 15:25:38 -0400

    Refresh sale board client cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "verns-estate-sale-warehouse-v20260803-grand-haven-sale";
+const CACHE_NAME = "verns-estate-sale-warehouse-v20260803-grand-haven-order";
 const APP_SHELL = [
   "./",
   "./index.html",
